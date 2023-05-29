0a8a831fb5d399db6863890fb148d5c74f128265
Vlad Krasovsky <krasovsky@kentik.com>
2023-05-29 04:09:37 -0700

    ec2 event an lambda working

--- infrastructure/deploy/lib/deploy-stack.ts
+++ infrastructure/deploy/lib/deploy-stack.ts
@@ -1,30 +1,69 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as events from 'aws-cdk-lib/aws-events';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as eventTargets from 'aws-cdk-lib/aws-events-targets';
 import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
+const DYNAMO_DB_PRIMARY_KEY = 'PK';
 export class DeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
+    // SQS
     const queue = new sqs.Queue(this, 'Ec2EventsQueue', {
       queueName: 'private-vpn-ec2-events-queue',
       visibilityTimeout: cdk.Duration.seconds(300),
     });
 
-    const handler = new lambda.Function(this, 'Ec2EventsLambdaConsumer', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      functionName: 'Ec2EventsLambdaConsumer',
-      code: lambda.Code.fromAsset('../lambda/ec2-events-consumer'),
-      handler: 'index.handler'
+    // DynamoDB
+    const dynamoDbTable = new dynamodb.Table(this, 'InstancesStateTable', {
+      partitionKey: {
+        name: DYNAMO_DB_PRIMARY_KEY,
+        type: dynamodb.AttributeType.STRING,
+      },
+      tableName: 'vpn-instances-state',
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      pointInTimeRecovery: true,
+      tableClass: dynamodb.TableClass.STANDARD_INFREQUENT_ACCESS,
     });
 
-    handler.addEventSource(new SqsEventSource(queue));
+    // Lambda
+    const lambdaFunction = new lambda.Function(
+      this,
+      'Ec2EventsLambdaConsumer',
+      {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        functionName: 'Ec2EventsLambdaConsumer',
+        code: lambda.Code.fromAsset('../lambda/ec2-events-consumer'),
+        handler: 'index.handler',
+        environment: {
+          PRIMARY_KEY: DYNAMO_DB_PRIMARY_KEY,
+          TABLE_NAME: dynamoDbTable.tableName,
+        },
+      }
+    );
 
-    this.exportValue(queue.queueUrl, { name: 'sqs-queue-url'});
+    dynamoDbTable.grantReadWriteData(lambdaFunction);
+    lambdaFunction.addEventSource(new SqsEventSource(queue));
+
+    const eventRule = new events.Rule(this, 'ec2-instance-state-change-event', {
+      enabled: true,
+      description: `Event to publich SQS Message when instance state changes`,
+      ruleName: `ec2-instance-state-change-event`,
+      eventPattern: {
+        source: ['aws.ec2'],
+        detailType: ['EC2 Instance State-change Notification'],
+        detail: {
+          'instance-id': ['i-08ac89f026edf5c9d', 'i-0140de9a9f8f9fe66'],
+        },
+      },
+      targets: [new eventTargets.SqsQueue(queue)],
+    });
+
+    this.exportValue(queue.queueUrl, { name: 'sqs-queue-url' });
   }
 }
--- infrastructure/lambda/ec2-events-consumer/index.js
+++ infrastructure/lambda/ec2-events-consumer/index.js
@@ -1,8 +1,71 @@
+const AWS = require('aws-sdk');
+
+const TABLE_NAME = process.env.TABLE_NAME || '';
+const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
+
+const RESERVED_RESPONSE = `Error: You're using AWS reserved keywords as attributes`;
+const DYNAMODB_EXECUTION_ERROR = `Error: Execution update, caused a Dynamodb error, please take a look at your CloudWatch Logs.`;
+
+const db = new AWS.DynamoDB.DocumentClient();
+
 const handler = async (event) => {
-    event.Records.forEach(record => {
-        const { body } = record;
-        console.log('aaaa', body);
-    })
-    return null;
-}
+  if ((event.Records?.length ?? 0) === 0) {
+    return { statusCode: 400, body: 'invalid request.' };
+  }
+
+  const record = event.Records[0];
+
+  const message = JSON.parse(record.body);
+  const { 'instance-id': instanceId, state: instanceState } = message.detail;
+
+  if (!instanceId) {
+    return {
+      statusCode: 400,
+      body: 'invalid request. Instance-id is not passed.',
+    };
+  }
+
+  if (!instanceState) {
+    return { statusCode: 400, body: 'invalid request. State is not passed.' };
+  }
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      [PRIMARY_KEY]: 'ec2Instance#' + instanceId,
+      instanceState: instanceState,
+      updatedAt: Math.round(Date.now() / 1000),
+    },
+    ConditionExpression: `attribute_not_exists(${PRIMARY_KEY})`,
+    ReturnValues: 'NONE',
+  };
+
+  try {
+    const putResponse = await db.put(params).promise();
+    return { statusCode: 204, body: putResponse };
+  } catch (dbError) {
+    if (dbError.code !== 'ConditionalCheckFailedException') {
+      return { statusCode: 500, body: dbError };
+    }
+  }
+
+  const updateParams = {
+    TableName: TABLE_NAME, // Replace with your DynamoDB table name
+    Key: { [PRIMARY_KEY]: 'ec2Instance#' + instanceId }, // Key of the existing record to update
+    UpdateExpression:
+      'SET instanceState = :instanceState, updatedAt = :updatedAt', // Update the desired attributes
+    ExpressionAttributeValues: {
+      ':instanceState': instanceState, // Replace with the updated values
+      ':updatedAt': Math.round(Date.now() / 1000),
+    },
+    ReturnValues: 'ALL_NEW', // Return the updated attributes
+  };
+
+  try {
+    const updateResponse = await db.update(updateParams).promise();
+    return { statusCode: 204, body: updateResponse };
+  } catch (dbError) {
+    return { statusCode: 500, body: dbError };
+  }
+};
 exports.handler = handler;
